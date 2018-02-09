00d77917ac7bab919f247d14cbb674086d6a076a
Ezequiel <ezequielrozen@gmail.com>
2018-02-08 23:24:16 -0300

    Add index file in models directory to import models

---
+++ src/app/models/index.js
@@ -0,0 +1,6 @@
+import Activity from './Activity'
+import AdminUser from './AdminUser'
+import Gym from './Gym'
+import Product from './Product'
+import Trainer from './Trainer'
+import User from './User'
